f9851ee65618f57b8f7f1b222e02dd88d3be7a94
juanpedroz <jzugbi@gmail.com>
2024-05-02 20:07:05 -0300

    creacion js productos, referencia y modificacion

---
+++ proyecto/js/productos.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let menuIcon = document.querySelector('.menu-icon');
+    let menu = document.querySelector('.menu');
+    let body = document.querySelector('body');
+    let modoToggle = document.querySelector('.modo-toggle input[type="checkbox"]');
+   
+
+
+    menuIcon.addEventListener('click', function () {
+        if (menu.classList.contains('show')) {
+            menu.classList.remove('show');
+        } else {
+            menu.classList.add('show');
+        }
+    });
+
+    modoToggle.addEventListener('change', function () {
+        if (body.classList.contains('modo-oscuro')) {
+            body.classList.remove('modo-oscuro');
+        } else {
+            body.classList.add('modo-oscuro');
+        }
+    });
+
+
+});
