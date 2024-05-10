19fc032f7a61c2f914252c4a852f1833b84a2018
juanpedroz <jzugbi@gmail.com>
2024-05-10 20:00:41 -0300

    tabla

---
+++ proyecto/js/tabla.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let body = document.querySelector('body');
+    let modoToggle = document.querySelector('.modo-toggle input[type="checkbox"]');
+    let menuIcon = document.querySelector('.menu-icon');
+    let menu = document.querySelector('.menu');
+    let mostrarImagenProducto = document.querySelector('.mostrarImagenProducto');
+    
+    // modoToggle.addEventListener('change', function () {
+    //     if (body.classList.contains('modo-oscuro')) {
+    //         body.classList.remove('modo-oscuro');
+    //     } else {
+    //         body.classList.add('modo-oscuro');
+    //     }
+    // });
+
+    mostrarImagenProducto.addEventListener('click', function (event) {
+       console.log(event.target.);
+    });
+
+
+
+});
