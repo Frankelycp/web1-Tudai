2004e9000da51149a93352e6a5b8e8bc5288fc56
juanpedroz <jzugbi@gmail.com>
2024-05-13 20:57:51 -0300

    dde todo

--- proyecto/js/pagina-principal.js
+++ proyecto/js/pagina-principal.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let lista = document.getElementById('lista');
-    let tablas = document.querySelectorAll('.tablas');
-    let tituloTabla = document.getElementById('tituloTabla');
+document.addEventListener("DOMContentLoaded", function () {
+    let lista = document.getElementById("lista");
+    let tablas = document.querySelectorAll(".tablas");
+    let tituloTabla = document.getElementById("tituloTabla");
 
-    lista.addEventListener('click', function(event) {
-        tituloTabla.classList.add('oculto');
+    lista.addEventListener("click", function(event) {
+        tituloTabla.classList.add("oculto");
 
         for (let i = 0; i < tablas.length; i++) {
-            tablas[i].classList.add('oculto');
+            tablas[i].classList.add("oculto");
         }
         let elemento = event.target.innerHTML;
-        let tablaMostrar = document.getElementById('tabla' + elemento);
+        let tablaMostrar = document.getElementById("tabla" + elemento);
         if (tablaMostrar) {
-            tablaMostrar.classList.remove('oculto');
-            tituloTabla.classList.remove('oculto');
-            tituloTabla.innerText = 'Mostrando:' + elemento;
+            tablaMostrar.classList.remove("oculto");
+            tituloTabla.classList.remove("oculto");
+            tituloTabla.innerText = "Mostrando:" + elemento;
         }
     });
     
